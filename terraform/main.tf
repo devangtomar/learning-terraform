218131572af05ed0653b9b0373326d1243e4d38a
devangtomar <devangtomar123@gmail.com>
2024-04-07 00:15:24 +0530

    Adding the ec2 instance back again..

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,20 +15,20 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# resource "aws_instance" "ec2_instance" {
-#   ami                    = "ami-0d0bed6857ceeda4b" # AMI for Amazon linux..
-#   instance_type          = "t2.micro"
-#   vpc_security_group_ids = [aws_security_group.instance.id]
-#   user_data              = <<-EOF
-#   #!/bin/bash
-#   echo 'Hello world!' > index.html
-#   nohup busybox httpd -f -p 8080 &
-#   EOF
+resource "aws_instance" "ec2_instance" {
+  ami                    = "ami-0d0bed6857ceeda4b" # AMI for Amazon linux..
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data              = <<-EOF
+  #!/bin/bash
+  echo 'Hello world!' > index.html
+  nohup busybox httpd -f -p 8080 &
+  EOF
 
-#   tags = {
-#     Name : "first-ec2-via-terraform"
-#   }
-# }
+  tags = {
+    Name : "first-ec2-via-terraform"
+  }
+}
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
