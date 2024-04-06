a1a534d0de7f1e0b351ec9045d4011f092efd893
devangtomar <devangtomar123@gmail.com>
2024-04-07 00:50:39 +0530

    update data.tf and main.tf

---
+++ terraform/security-group.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+  ingress {
+    to_port     = 80
+    from_port   = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    to_port     = 0
+    from_port   = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
