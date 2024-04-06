2b1d06beb4e4dbed797ea6346ea1417f1231042e
devangtomar <devangtomar123@gmail.com>
2024-04-07 00:12:00 +0530

    update .gitignore and main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,11 +15,32 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "ec2_instance" {
-  ami           = "abc"
-  instance_type = "t2.micro"
+# resource "aws_instance" "ec2_instance" {
+#   ami                    = "ami-0d0bed6857ceeda4b" # AMI for Amazon linux..
+#   instance_type          = "t2.micro"
+#   vpc_security_group_ids = [aws_security_group.instance.id]
+#   user_data              = <<-EOF
+#   #!/bin/bash
+#   echo 'Hello world!' > index.html
+#   nohup busybox httpd -f -p 8080 &
+#   EOF
 
-  tags = {
-    Name : "first-ec2"
+#   tags = {
+#     Name : "first-ec2-via-terraform"
+#   }
+# }
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+output "id" {
+  # value = aws_instance.ec2_instance.id
+  value = aws_security_group.instance.id
 }
