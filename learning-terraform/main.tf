719cccf6f621fad544308c97a738cae732d750ce
devangtomar <devangtomar123@gmail.com>
2024-05-04 17:14:47 +0530

    update main.tf

--- learning-terraform/main.tf
+++ learning-terraform/main.tf
@@ -1,7 +1,33 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
+      source = "hashicorp/aws"
+      # version = ~> 4.16" ~ no version is specified which makes it pick the latest version..
     }
   }
+  # required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Basic instance"
+  }
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/21"
+  tags = {
+    Name = "green"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.6.0.0/23"
 }
