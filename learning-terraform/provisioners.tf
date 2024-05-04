14ee467320872590521fad619d36628d1dde56ed
devangtomar <devangtomar123@gmail.com>
2024-05-04 23:53:07 +0530

    Adding info about provisioners..

---
+++ learning-terraform/provisioners.tf
@@ -0,0 +1,23 @@
+resource "aws_instance" "instance_1" {
+  provisioner "file" {
+    source      = "./data.tf"
+    destination = "/home/ubuntu/data.tf"
+  }
+  provisioner "file" {
+    content     = "I want to copy this string to the destination"
+    destination = "/home/ubuntu/test.txt"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "touch hello.txt",
+      "echo helloworld remote-exec provisioner >> hello.txt",
+    ]
+  }
+}
+
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command     = "'This is test file for null resource local-exec' >>  nullresource-generated.txt"
+    interpreter = ["PowerShell", "-Command"]
+  }
+}
