83ed3cb95229fc10a67e544acd0e36e7af5adbe2
devangtomar <devangtomar123@gmail.com>
2025-11-30 02:24:56 +0530

    Adding about templates and remote backend.

---
+++ terraform-on-aws/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-state"
+    key = "key/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
